bf25c85d60c28d13e70b5e9cfc2677a4c186d4a4
Eom Yunseop <lh0156@naver.com>
2022-09-16 16:58:38 +0900

    Create RemoveDuplicatesfromSortedArray Class FIle

---
+++ src/Array/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class RemoveDuplicatesfromSortedArray {
+
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 0, 0, 0, 1, 2, 3, 4, 5};
+
+        int index = 1;
+
+        for (int i=0; i<nums.length-1; ++i) {
+
+            if (nums[i] != nums[i+1]) {
+                nums[index] = nums[i+1];
+                index++;
+            }
+
+            //0 0 1 2 3
+        }
+
+        System.out.println(index);
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        return 0;
+    }
+}
