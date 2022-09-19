4070bbd2d5d6dca41ecaa57ae2eef9f1f2eb2568
Eom Yunseop <lh0156@naver.com>
2022-09-19 10:00:30 +0900

    Update UUIDTest.java

--- src/Array/UUIDTest.java
+++ src/Array/UUIDTest.java
@@ -6,11 +6,32 @@ public class UUIDTest {
 
     public static void main(String[] args) {
 
+//        System.out.println(version1());
+        System.out.println(version2());
+
+    }
+
+    public static long version1() {
+
+        long beforeTime = System.currentTimeMillis();
         String temp1 = UUID.randomUUID().toString();
-        System.out.println(temp1);
+        long afterTime = System.currentTimeMillis();
+        long result = afterTime - beforeTime;
+
+       return result;
+    }
+
+    public static long version2() {
+
+        long beforeTime = System.currentTimeMillis();
 
         String temp2 = UUID.fromString("3051a8d7-aea7-1801-e0bf-bc539dd60cf3").toString();
-        System.out.println(temp2);
+
+        long afterTime = System.currentTimeMillis();
+
+        long result = afterTime - beforeTime;
+
+        return result;
 
     }
 
