879b677d3064dd3635491a16502a8a25b6dc0b23
Eom Yunseop <lh0156@naver.com>
2022-11-28 16:19:42 +0900

    Update Algorithm

--- src/baekjoon/Okunsoo.java
+++ src/baekjoon/Okunsoo.java
@@ -3,25 +3,47 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Okunsoo {
 
+    /*
+            3 5 2 7
+    * 1. Stack은 index를 저장해두는 공간이다.
+    * 2. stack에 0이 들어간다
+    *   2.1. number[0]이 5보다 작으므로 number[stack]은 i
+    * */
     public static void main(String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        3 5 2 7
 
-        String temp = reader.readLine();
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String temp = "3 5 2 7";
         String[] sArr = temp.split(" ");
+        Stack<Integer> stack = new Stack();
 
-        int[] number = new int[s.length];
+        int[] number = new int[sArr.length];
+        int[] answer = new int[sArr.length];
 
-        for (int i=0; i<sArr.length-1; ++i) {
-            number[i] = Integer.parseInt(sArr[i]);
+        for (int j=0; j<answer.length; ++j) {
+            answer[j] = -1;
         }
 
+        number[0] = Integer.parseInt(sArr[0]);
+        stack.push(0);
 
+        for(int i=1; i<sArr.length; ++i) {
 
+            int n = Integer.parseInt(sArr[i]);
+            number[i] = n;
+
+            while(!stack.isEmpty() && number[stack.peek()] < n) {
+                answer[stack.pop()] = n;
+            }
+            stack.push(i);
+        }
 
     }
 }
---
+++ src/baekjoon/b200/Q9093.java
@@ -0,0 +1,48 @@
+package baekjoon.b200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * 문장이 주어졌을 때, 단어를 모두 뒤집어서 출력하는 프로그램을 작성하시오. 단, 단어의 순서는 바꿀 수 없다. 단어는 영어 알파벳으로만 이루어져 있다.
+ * 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 문장이 하나 주어진다. 단어의 길이는 최대 20, 문장의 길이는 최대 1000이다. 단어와 단어 사이에는 공백이 하나 있다.
+ *
+ * 1. 버퍼로 정수 값을 입력 받는다.
+ * 2. 정수의 수만큼 for문을 돌아준다.
+ * 3. 정수값을 split으로 잘라준다.
+ * 4. split으로 잘라준 갯수만큼 for문을 돈다.
+ * 5. 잘라준 temp의 각 배열의 방의 갯수만큼 반대로 돌면서 sb에 추가해준다.
+ */
+
+public class Q9093 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int count = Integer.parseInt(reader.readLine());
+
+        for(int i=0; i<count; ++i) {
+
+            String[] temp = reader.readLine().split(" ");
+
+            for(int j=0; j<temp.length; ++j) {
+
+
+                sb.append(new StringBuilder(temp[j]).reverse() + " ");
+
+            }
+
+            sb.append("\n");
+
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+}
