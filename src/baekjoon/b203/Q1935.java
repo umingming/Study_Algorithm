c56894265006440f29326db1633535d78524c531
Eom Yunseop <lh0156@naver.com>
2023-01-13 13:48:49 +0900

    Update Question File

---
+++ src/baekjoon/b203/Q1935.java
@@ -0,0 +1,38 @@
+package baekjoon.b203;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 후위 표기식과 각 피연산자에 대응하는 값들이 주어져 있을 때, 그 식을 계산하는 프로그램을 작성하시오.
+ *
+ * 5
+ * ABC*+DE/-
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ *
+ * 6.20
+ *
+ *
+ */
+public class Q1935 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(reader.readLine());
+        String temp = reader.readLine();
+
+
+
+
+
+
+
+    }
+
+}
