b5bf8367ce5cf72a9a3cdbf7ec15a95d11b53788
Eom Yunseop <lh0156@naver.com>
2023-02-06 13:52:55 +0900

    버블 정렬으로 접미사를 정렬했는데 틀렸다고 나온다.
    왠지 직접 구현한 정렬 알고리즘이 아닌 자바의 프로토 타입 메소드를 사용해야 할 삘이군

---
+++ src/baekjoon/b203_dataconstruct/Q11656.java
@@ -0,0 +1,79 @@
+package baekjoon.b203_dataconstruct;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 접미사
+ *
+ * [문제]
+ * 접미사 배열은 문자열 S의 모든 접미사를 사전순으로 정렬해 놓은 배열이다.
+ *
+ * baekjoon의 접미사는 baekjoon, aekjoon, ekjoon, kjoon, joon, oon, on, n 으로 총 8가지가 있고
+ * 이를 사전순으로 정렬하면, aekjoon, baekjoon, ekjoon, joon, kjoon, n, on, oon이 된다.
+ *
+ * 문자열 S가 주어졌을 때, 모든 접미사를 사전순으로 정렬한 다음 출력하는 프로그램을 작성하시오.
+ *
+ * [입력]
+ * baekjoon
+ *
+ * [결과]
+ * aekjoon
+ * baekjoon
+ * ekjoon
+ * joon
+ * kjoon
+ * n
+ * on
+ * oon
+ */
+public class Q11656 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = reader.readLine();
+        String[] arr = new String[input.length()];
+
+        for(int i=0; i<arr.length; ++i) {
+
+            arr[i] = input.substring(i);
+
+        }
+
+        bubbleSort(arr);
+
+        for (String s : arr) {
+
+            System.out.println(s);
+        }
+
+
+
+    }
+
+    public static void bubbleSort (String[] arr) {
+        bubbleSort(arr, arr.length-1);
+    }
+
+    public static void bubbleSort (String[] arr, int lastIndex) {
+        if (lastIndex > 0) {
+
+            for (int i=1; i<=lastIndex; ++i) {
+
+                if (arr[i-1].charAt(0) >= arr[i].charAt(0)) {
+                    String temp = arr[i-1];
+                    arr[i-1] = arr[i];
+                    arr[i] = temp;
+                }
+
+            }
+
+            bubbleSort(arr, lastIndex-1);
+
+        }
+    }
+
+}
