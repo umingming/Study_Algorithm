6d9fa3b1d916c16371132dc5c83935826456f4e1
Eom Yunseop <lh0156@naver.com>
2023-03-07 16:59:59 +0900

    스텍 구현 연습

---
+++ src/datastruct/stack/Stack2.java
@@ -0,0 +1,42 @@
+package datastruct.stack;
+
+import java.util.EmptyStackException;
+
+public class Stack2<T> {
+    static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        public Node(T item) {
+            this.item = item;
+        }
+    }
+
+    private Node<T> last;
+
+    public T pop() {
+        if (last == null) {
+            throw new EmptyStackException();
+        }
+
+        T item = last.item;
+        last = last.next;
+        return item;
+
+    }
+
+    public T peek() {
+        if (last == null) {
+            throw new EmptyStackException();
+        }
+
+        return last.item;
+    }
+
+//    public void push(T item) {
+//        Node<T> item = new Node<T>(item);
+//
+//        last.next = last;
+//    }
+
+}
