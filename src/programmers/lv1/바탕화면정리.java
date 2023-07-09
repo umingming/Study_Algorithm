c5595e1d637659b91b3b7927eb3285210a80e911
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-07-09 09:30:06 +0900

    feat: 코테 문제 업로드

---
+++ src/programmers/lv1/바탕화면정리.java
@@ -0,0 +1,67 @@
+package programmers.lv1;
+
+public class 바탕화면정리 {
+
+    /**
+     * 코딩테스트를 준비하는 머쓱이는 프로그래머스에서 문제를 풀고 나중에 다시 코드를 보면서 공부하려고 작성한 코드를 컴퓨터 바탕화면에 아무 위치에나 저장해 둡니다.
+     * 저장한 코드가 많아지면서 머쓱이는 본인의 컴퓨터 바탕화면이 너무 지저분하다고 생각했습니다. 프로그래머스에서 작성했던 코드는 그 문제에 가서 다시 볼 수 있기 때문에 저장해 둔 파일들을 전부 삭제하기로 했습니다.
+     *
+     * 컴퓨터 바탕화면은 각 칸이 정사각형인 격자판입니다. 이때 컴퓨터 바탕화면의 상태를 나타낸 문자열 배열 wallpaper가 주어집니다.
+     * 파일들은 바탕화면의 격자칸에 위치하고 바탕화면의 격자점들은 바탕화면의 가장 왼쪽 위를 (0, 0)으로 시작해 (세로 좌표, 가로 좌표)로 표현합니다.
+     * 빈칸은 ".", 파일이 있는 칸은 "#"의 값을 가집니다. 드래그를 하면 파일들을 선택할 수 있고, 선택된 파일들을 삭제할 수 있습니다.
+     *
+     * 머쓱이는 최소한의 이동거리를 갖는 한 번의 드래그로 모든 파일을 선택해서 한 번에 지우려고 하며 드래그로 파일들을 선택하는 방법은 다음과 같습니다.
+     *
+     * 드래그는 바탕화면의 격자점 S(lux, luy)를 마우스 왼쪽 버튼으로 클릭한 상태로 격자점 E(rdx, rdy)로 이동한 뒤 마우스 왼쪽 버튼을 떼는 행동입니다. 이때, "점 S에서 점 E로 드래그한다"고 표현하고 점 S와 점 E를 각각 드래그의 시작점, 끝점이라고 표현합니다.
+     *
+     * 점 S(lux, luy)에서 점 E(rdx, rdy)로 드래그를 할 때, "드래그 한 거리"는 |rdx - lux| + |rdy - luy|로 정의합니다.
+     *
+     * 점 S에서 점 E로 드래그를 하면 바탕화면에서 두 격자점을 각각 왼쪽 위, 오른쪽 아래로 하는 직사각형 내부에 있는 모든 파일이 선택됩니다.
+     *
+     * 예를 들어 wallpaper = [".#...", "..#..", "...#."]인 바탕화면을 그림으로 나타내면 다음과 같습니다.
+     *
+     * 이러한 바탕화면에서 다음 그림과 같이 S(0, 1)에서 E(3, 4)로 드래그하면 세 개의 파일이 모두 선택되므로 드래그 한 거리 (3 - 0) + (4 - 1) = 6을 최솟값으로 모든 파일을 선택 가능합니다.
+     *
+     * [풀이]
+     * 1. lux, luy, rdx, rdy 변수를 선언한다.
+     *  1.1. lux와 luy는 최초 1회에만, rdx, rdy는 y값이 크면 쭉쭉 할당해준다.
+     * 2. 존재하는 최대최소 xy값을 찾으면 될 듯
+     */
+    public static void main(String[] args) {
+
+        String[] wallpaper = {"..", "#."};
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
+
+        for (int i=0; i<wallpaper.length; ++i) {
+
+            char[] chars = wallpaper[i].toCharArray();
+
+            for (int j = 0; j< chars.length; ++j) {
+                if (chars[j] == '#') {
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
+                }
+            }
+        }
+
+        ++rdx;
+        ++rdy;
+
+        System.out.println(lux);
+        System.out.println(luy);
+        System.out.println(rdx);
+        System.out.println(rdy);
+
+    }
+
+//    public int[] solution(String[] wallpaper) {
+//        int[] answer = {};
+//        return answer;
+//    }
+
+}
