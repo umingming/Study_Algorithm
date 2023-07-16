9758b69ee1bb6d5975fc9cdc0e5cfa6e464dc841
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-07-16 15:16:37 +0900

    feat: 이차원 배열 연습

--- src/technical/이차원배열연습.java
+++ src/technical/이차원배열연습.java
@@ -1,5 +1,7 @@
 package technical;
 
+import java.util.Arrays;
+
 /**
  * 일반적으로 x축 y축 개념으로 이차원 배열을 생각하다보니
  * 가끔 [x][y] 축으로 착각하기도 하는 것 같다.
@@ -14,6 +16,7 @@ public class 이차원배열연습 {
             temp[i] = new int[]{i*10+0, i*10+1, i*10+2, i*10+3, i*10+4, i*10+5, i*10+6, i*10+7, i*10+8, i*10+9};
         }
 
+        System.out.println(Arrays.toString(temp[0]));
         System.out.println(temp[0][1]);
         System.out.println(temp[1][1]);
 
