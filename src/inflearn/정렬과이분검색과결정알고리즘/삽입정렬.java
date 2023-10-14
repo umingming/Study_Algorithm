661adad23e84bafb622e65f74b4bcbf7a3e99fe5
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-10-14 23:55:38 +0900

    feat: 삽입정렬

---
+++ src/inflearn/정렬과이분검색과결정알고리즘/삽입정렬.java
@@ -0,0 +1,30 @@
+package inflearn.정렬과이분검색과결정알고리즘;
+
+public class 삽입정렬 {
+
+    public static void sort(int[] arr) {
+        for(int i=1; i<arr.length; ++i) {
+            int temp = arr[i];
+            int j;
+            for(j=i-1; j>=0; --j) {
+                if (arr[j] > temp) {
+                    arr[j+1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j+1] = temp;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {11, 7, 5, 6, 10, 9};
+        sort(arr);
+
+        for (int i : arr) {
+            System.out.print(i + " -> ");
+        }
+    }
+
+}
