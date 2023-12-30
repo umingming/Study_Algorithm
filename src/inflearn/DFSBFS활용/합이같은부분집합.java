aa094b7fe728e39b88c4097b90c0f599c292e82c
YunSeop <62834176+lh0156@users.noreply.github.com>
2023-12-30 16:14:18 +0900

    feat: 합이 같은 부분집합 DFS

---
+++ src/inflearn/DFSBFS활용/합이같은부분집합.java
@@ -0,0 +1,48 @@
+package inflearn.DFSBFS활용;
+
+import java.util.Arrays;
+
+/**
+ * 설명
+ *
+ * N개의 원소로 구성된 자연수 집합이 주어지면, 이 집합을 두 개의 부분집합으로 나누었을 때
+ *
+ * 두 부분집합의 원소의 합이 서로 같은 경우가 존재하면 “YES"를 출력하고, 그렇지 않으면 ”NO"를 출력하는 프로그램을 작성하세요.
+ *
+ * 둘로 나뉘는 두 부분집합은 서로소 집합이며, 두 부분집합을 합하면 입력으로 주어진 원래의 집합이 되어 합니다.
+ *
+ * 예를 들어 {1, 3, 5, 6, 7, 10}이 입력되면 {1, 3, 5, 7} = {6, 10} 으로 두 부분집합의 합이 16으로 같은 경우가 존재하는 것을 알 수 있다.
+ */
+public class 합이같은부분집합 {
+
+    static int total;
+    static boolean flag = false;
+    static String answer = "No";
+    static int n = 6;
+    public static void DFS(int l, int sum, int[] arr) {
+        if (flag || sum>total/2) {
+            return;
+        }
+
+        if (l == n) {
+            if (total - sum == sum) {
+                flag = true;
+                answer = "Yes";
+            }
+        } else {
+            DFS(l+1, sum+arr[l], arr);
+            DFS(l+1, sum, arr);
+        }
+    }
+
+    public static void DFS(int[] arr) {
+        DFS(0, 0, arr);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6, 7, 10};
+        total = Arrays.stream(arr).sum();
+        DFS(arr);
+        System.out.println(answer);
+    }
+}
