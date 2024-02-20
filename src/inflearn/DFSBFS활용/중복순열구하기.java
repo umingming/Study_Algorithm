85d5f16841728307bd9c114e6ea2c09e95cf273d
YunSeop <62834176+lh0156@users.noreply.github.com>
2024-02-21 00:14:12 +0900

    feat: 중복 순열 구하기

--- src/inflearn/DFSBFS활용/중복순열구하기.java
+++ src/inflearn/DFSBFS활용/중복순열구하기.java
@@ -1,52 +1,37 @@
 package inflearn.DFSBFS활용;
 
-import inflearn.재귀함수트리그래프.DFS;
-
 /**
  * 1부터 N까지 번호가 적힌 구슬이 있습니다.
- * 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열하는 방법을 모두 출력합니다.
+ * 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열 하는 방법을 모두 출력합니다.
  *
- * [입력 설명]
- * 첫 번째 줄에 자연수 N(3<=N<=10)과 M(2<=M<=N)이 주어집니다.
- *
- * [출력 설명]
- * 첫 번째 줄에 결과를 출력합니다.
- * 출력 순서는 사전순으로 오름차순으로 출력합니다.
- *
- * [입력 예제 1]
- * 3 2
- *
- * [출력 예제 1]
- * 1 1
- * 1 2
- * 1 3
- * 2 1
- * 2 2
- * 2 3
- * 3 1
- *
- * [풀이]
- *
- *
+ * 1 2 3
  */
 public class 중복순열구하기 {
-    static int[] pm;
-    static int n = 3;
-    static int m = 2;
-    public static void DFS(int L) {
+
+    static int n, m;
+    static int[] arr;
+
+    public void solution(int L) {
         if (L == m) {
-            System.out.println(pm[0] + " " + pm[1]);
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
         } else {
-            for(int i=1; i<=3; ++i) {
-                pm[L] = i;
-                DFS(L+1);
+            for(int i=1; i<=n; ++i) {
+                arr[L] = i;
+                solution(L+1);
             }
         }
     }
 
     public static void main(String[] args) {
-        pm = new int[m];
-
-        DFS(0);
+        중복순열구하기 solution = new 중복순열구하기();
+        n = 3;
+        m = 2;
+        arr = new int[m];
+        solution.solution(0);
     }
+
 }
