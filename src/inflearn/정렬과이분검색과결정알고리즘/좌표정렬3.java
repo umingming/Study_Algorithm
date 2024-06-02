64c5eb06938e22a8a903546c809490fc4495fa91
YunSeop <62834176+lh0156@users.noreply.github.com>
2024-06-02 16:53:00 +0900

    feat: 좌표 정렬 3

---
+++ src/inflearn/정렬과이분검색과결정알고리즘/좌표정렬3.java
@@ -0,0 +1,85 @@
+package inflearn.정렬과이분검색과결정알고리즘;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 7. 좌표 정렬
+ * 설명
+ *
+ * N개의 평면상의 좌표(x, y)가 주어지면 모든 좌표를 오름차순으로 정렬하는 프로그램을 작성하세요.
+ *
+ * 정렬기준은 먼저 x값의 의해서 정렬하고, x값이 같을 경우 y값에 의해 정렬합니다.
+ *
+ *
+ * 입력
+ * 첫째 줄에 좌표의 개수인 N(3<=N<=100,000)이 주어집니다.
+ *
+ * 두 번째 줄부터 N개의 좌표가 x, y 순으로 주어집니다. x, y값은 양수만 입력됩니다.
+ *
+ *
+ * 출력
+ * N개의 좌표를 정렬하여 출력하세요.
+ *
+ *
+ * 예시 입력 1
+ *
+ * 5
+ * 2 7
+ * 1 3
+ * 1 2
+ * 2 5
+ * 3 6
+ * 예시 출력 1
+ *
+ * 1 2
+ * 1 3
+ * 2 5
+ * 2 7
+ * 3 6
+ */
+public class 좌표정렬3 {
+
+    static class Point implements Comparable<Point>{
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if (this.x == o.x) {
+                return this.y - o.y;
+            } else {
+                return this.x - o.x;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /**
+         *  * 2 7
+         *  * 1 3
+         *  * 1 2
+         *  * 2 5
+         *  * 3 6
+         */
+        List<Point> list = new ArrayList<>();
+        list.add(new Point(2, 7));
+        list.add(new Point(1, 3));
+        list.add(new Point(1, 2));
+        list.add(new Point(2, 5));
+        list.add(new Point(3, 6));
+
+        Collections.sort(list);
+
+        for(Point point : list) {
+            System.out.print(point.x + " " + point.y);
+            System.out.println();
+        }
+    }
+}
