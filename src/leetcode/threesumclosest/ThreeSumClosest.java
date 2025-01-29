5ffbadd037d2aa6a9098cac7757755349db212dc
엄윤섭 <62834176+lh0156@users.noreply.github.com>
2025-01-29 20:29:30 +0900

    fix: Practice Two Point Algorithm

--- src/leetcode/threesumclosest/ThreeSumClosest.java
+++ src/leetcode/threesumclosest/ThreeSumClosest.java
@@ -1,4 +1,4 @@
-package leetcode;
+package leetcode.threesumclosest;
 
 import java.util.Arrays;
 
---
+++ src/leetcode/threesumclosest/ThreeSumClosest2.java
@@ -0,0 +1,55 @@
+package leetcode.threesumclosest;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+ *
+ * Return the sum of the three integers.
+ *
+ * You may assume that each input would have exactly one solution.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,2,1,-4], target = 1
+ * Output: 2
+ * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ * Example 2:
+ *
+ * Input: nums = [0,0,0], target = 1
+ * Output: 0
+ * Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ *
+ *
+ * Constraints:
+ *
+ * 3 <= nums.length <= 500
+ * -1000 <= nums[i] <= 1000
+ * -104 <= target <= 104
+ */
+public class ThreeSumClosest2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        int closestNum = nums[0] + nums[1] + nums[2];
+
+        for(int i=0; i<nums.length-2; ++i) {
+
+            int left = i+1;
+            int right = right = nums.length-1;
+
+            while(left < right) {
+
+            }
+
+        }
+    }
+
+}
