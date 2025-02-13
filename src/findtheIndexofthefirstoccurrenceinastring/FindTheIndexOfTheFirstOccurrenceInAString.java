22719b7ed85cc459edc1ceeb147d3038647d1230
엄윤섭 <62834176+lh0156@users.noreply.github.com>
2025-02-13 21:53:49 +0900

    fix: FindTheIndexOfTheFirstOccurrenceInAString

---
+++ src/findtheIndexofthefirstoccurrenceinastring/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,13 @@
+package findtheIndexofthefirstoccurrenceinastring;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public static void main(String[] args) {
+        FindTheIndexOfTheFirstOccurrenceInAString solution = new FindTheIndexOfTheFirstOccurrenceInAString();
+    }
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
